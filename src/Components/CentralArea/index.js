28ac9d0a1f928fffd25c10afdf7d82ed8e44962b
LToomre <LToomre@gmail.com>
2018-10-09 14:24:46 -0700

    Saving small changes

--- src/Components/CentralArea/index.js
+++ src/Components/CentralArea/index.js
@@ -1,6 +1,7 @@
-import React from "react";
-import Grid from "@material-ui/core/Grid";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 import LeftPane from "./LeftPane";
 import RightPane from "./RightPane";
 
--- src/Components/Layouts/header.js
+++ src/Components/Layouts/header.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+
 import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
 import LocomotiveDialog from "../Dialogs/LocomotiveDialog";
 
-export default ({ locomotive, locomotives, railroads, onLocomotiveCreate }) => {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h4" color="inherit" style={{ flex: 1 }}>
-          Steam Locomotives
-        </Typography>
+let styles;
 
-        <LocomotiveDialog
-          locomotive={locomotive}
-          locomotives={locomotives}
-          onLocomotiveCreate={onLocomotiveCreate}
-          railroads={railroads}
-        />
-      </Toolbar>
-    </AppBar>
-  );
+class Header extends React.Component {
+  render() {
+    const {
+      classes,
+      locomotive,
+      locomotives,
+      railroads,
+      onLocomotiveCreate
+    } = this.props;
+
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4" color="inherit" style={{ flex: 1 }}>
+            Steam Locomotives
+          </Typography>
+
+          <LocomotiveDialog
+            locomotive={locomotive}
+            locomotives={locomotives}
+            onLocomotiveCreate={onLocomotiveCreate}
+            railroads={railroads}
+          />
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+styles = theme => ({});
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired
 };
+
+Header.defaultProps = {};
+
+export default withStyles(styles)(Header);
